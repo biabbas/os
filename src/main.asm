c1b12411b8952940a316585d541bb6f27fcefc78
B I <bimohammadabbas@gmail.com>
2024-05-23 15:32:31 +0530

    Adding printing function after setup of registers

--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,36 @@
 org 0x7C00
 bits 16
 
+start:
+    jmp main
+;
+; Print a string to screen
+; Params
+; ds:si points to the string
+
+puts:
+    ; save registers
+    push si
+    push ax
+.loop:
+    loadsb  ;loads next character in si to al and increments si
+    or al, al
+    jz .done
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
 main:
+    ; setup data
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    
     hlt
 
 .halt:
