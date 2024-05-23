ef332f21937c22fe8a5fc36e5831585021a00906
B I <bimohammadabbas@gmail.com>
2024-05-23 15:48:53 +0530

    Printing the welcome message

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,8 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
 start:
     jmp main
 ;
@@ -13,9 +15,13 @@ puts:
     push si
     push ax
 .loop:
-    loadsb  ;loads next character in si to al and increments si
+    lodsb  ;loads next character in si to al and increments si
     or al, al
     jz .done
+
+    mov ah, 0x0e; Bios interrupt for tty mode screen
+    mov bh, 0 ; page number to zero
+    int 0x10
     jmp .loop
 
 .done:
@@ -30,12 +36,16 @@ main:
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
-    
+
+    mov si, msg_hello
+    call puts
+
     hlt
 
 .halt:
     jmp .halt
 
+msg_hello: db 'Welcome to the den',ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
